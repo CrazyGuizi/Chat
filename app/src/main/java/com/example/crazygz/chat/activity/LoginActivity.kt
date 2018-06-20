5b5a509e008791296641239504b56538691c3426
李德贵 <1012610779@qq.com>
2018-06-20 09:49:00 +0800

    初始化

---
+++ app/src/main/java/com/example/crazygz/chat/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.crazygz.chat.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crazygz.chat.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
